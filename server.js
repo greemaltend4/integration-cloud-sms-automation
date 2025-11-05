dacbe9322b524d95cee3d2d090dc395706687a10
greemaltend4 <dengestabiy@hotmail.com>
2025-11-05 11:41:36 +0000

    Added additional console log for better debugging during MongoDB connection.

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ app.use(express.urlencoded({ extended: true }));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
+  .then(() => {
+    console.log('MongoDB connected');
+    // Log a message indicating the connection was successful
+    console.log('Successfully connected to MongoDB');
+  })
   .catch(err => console.error('Error connecting to MongoDB:', err));
 
 // Routes
